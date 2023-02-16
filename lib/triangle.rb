050905f6e515f0e95a88c54f72f1f41c31c6a6fe
Ruth Moraa <moraar37@gmail.com>
2023-02-16 14:35:32 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
   # write code here
+
+
+  attr_accessor :right, :left, :base
+
+  def initialize(right, left, base)
+    @right = right
+    @left = left
+    @base = base
+  end
+
+  class TriangleError < StandardError
+  end
+  
+  def kind
+    validate_triangle
+    if right == left && left == base
+      :equilateral
+    elsif right == left || left == base || right == base
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    [right, left, base].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    right + left > base && right + base > left && left + base > right
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+
 end
